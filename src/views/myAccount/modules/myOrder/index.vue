142c851182b2de4e3d72e86557f053f160b5a88c
chenshengning <1185451124@qq.com>
2020-05-16 09:39:17 +0800

    '我的订单静态页面'

--- src/views/myAccount/modules/myOrder/index.vue
+++ src/views/myAccount/modules/myOrder/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class='my_order'>
+      <!-- 状态筛选 -->
       <div class='order_nav'>
           <p 
           v-for='(item, index) in navList'
@@ -9,6 +10,7 @@
           <el-select></el-select>
           <el-select></el-select>
       </div>
+      <!-- 订单表头 -->
       <div class='order_head'>
           <div class='order_head_left'>
               商品
@@ -21,12 +23,32 @@
               <span>操作</span>
           </div>
       </div>
-
+    <!-- 订单列表 -->
       <div class='order_list_wrap'>
-          <div class='order_item'>
+          <!-- 单个订单组件 -->
+          <div class='order_item' v-for='i in 10' :key='i'>
             <div class='order_item_head'>
-                <p>订单编号: <span>{{"item.orderNum"}}</span></p>
+                <p>订单编号: <span>{{item.orderNum}}</span></p>
                 <p>联系卖家</p>
+            </div>
+            <div class="order_item_body">
+                <div class='item_body_left'>
+                    <img class='goods_img' :src="require('../../../../assets/goods.png')">
+                    <div class='goods_text'>
+                        <p>{{'商品名称'.repeat(5)}}</p>
+                        <p>
+                            <span>拼</span>
+                            <span>可售时间段{{20180912}}至{{20190603}}</span>
+                        </p>
+                    </div>
+                </div>
+                <div class="item_body_right">
+                    <span>19999.00</span>
+                    <span>99999</span>
+                    <span>17450.00</span>
+                    <span>待付款</span>
+                    <span>退款</span>
+                </div>
             </div>
         </div>
       </div>
@@ -38,6 +60,9 @@ export default {
     data () {
         return {
             navTabIndex: 0,
+            item: {
+                orderNum:195458495669
+            },
             navList: [
                 {title: '全部订单'},
                 {title: '待付款'},
@@ -100,6 +125,8 @@ export default {
         padding-right: 21px;
         .order_item {
             height: 180px;
+            border: 1px solid #EAF8FF;
+            margin-bottom: 12px;
             .order_item_head {
                 display: flex;
                 justify-content: space-between;
@@ -116,6 +143,42 @@ export default {
                     background-size: 18px 17px;
                     background-position: 0 12px;
                     cursor: pointer;
+                }
+            }
+            .order_item_body {
+                display: flex;
+                height: 140px;
+                color: #333;
+                .item_body_left {
+                    display: flex;
+                    width: 393px;
+                    height: 140px;
+                    padding-top: 29px;
+                    padding-left: 23px;
+                    .goods_img {
+                        width: 80px;
+                        height: 80px;
+                        margin-right: 16px;
+                    }
+                    .goods_text {
+                        flex: 1;
+                        p:first-child {
+                            font-size: 16px;
+                            margin-bottom: 20px;
+                            padding-right: 44px;
+                        }
+                        p:last-child {
+                            font-size: 14px;
+                        }
+                    }
+                }
+                .item_body_right {
+                    padding-top: 30px;
+                    text-align: center;
+                    display: flex;
+                    justify-content: space-between;
+                    width: 491px;
+                    height: 140px;
                 }
             }
         }
