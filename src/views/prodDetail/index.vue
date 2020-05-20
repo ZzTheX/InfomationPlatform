a311bc98858994f526f1267409027ba6a8dfc154
chenshengning <1185451124@qq.com>
2020-05-20 23:56:54 +0800

    prod-detail

--- src/views/prodDetail/index.vue
+++ src/views/prodDetail/index.vue
@@ -12,11 +12,35 @@
                 <p class='prod_title'>OLAY熬夜黄金酵母面膜菁醇青春密集修护面膜女补水保湿滋润烟酰胺OLAY熬夜黄金酵母面膜菁醇</p>
                 <div class='price_info'>
                     <p class='price'>预售价格<span>{{" ￥799.00"}}</span></p>
+                    <p class='sell_time'>预售时间：2020-02-01至2020-3-12</p>
+                    <p class='store_info'>
+                      <span>剩余数量:999</span>
+                      <span>已售数量：1000000</span>
+                      <span>总数量：10000</span>
+                    </p>
+                    <p class='earnest_money'>
+                      <span>最小起批数量：1</span>
+                      <span>期望定金：￥5000</span>
+                    </p>
+                    <hr style='color:#E6E6E6;margin-bottom:20px'/>
+                    <p class='groupon_info'>
+                      <span>支持散装拼团</span>
+                      <span>单人(地址)最小购买数量：2</span>
+                      <span>最低成团数量：50000</span>
+                    </p>
+                    <hr style='color:#E6E6E6;margin-bottom:14px'>
+                    <p>最晚成团日期：2020.02.02</p>
+                    <div class='icons'>
+                      <p>
+                        <span>999人想要</span>
+                        <span>超赞999</span>
+                        <span>浏览563</span>
+                        <span>关注</span>
+                      </p>
+                      <p>四川成都</p>
+                    </div>
                 </div>
-                <div class='store_info'>
-
-                </div>
-                <div class='operate'></div>
+                
             </div>
       </div>
       <div class='more_info'>
@@ -25,7 +49,12 @@
                 <img src="../../assets/company.png" width="80" height="80">
               </div>
             </div>
-            <div class='prod_description'></div>
+            <div class='prod_description'>
+              <div class='description_head'>
+                <span @click='toggle' class='active'>宝贝详情</span>
+                <span @click='toggle'>宝贝留言</span>
+              </div>
+            </div>
       </div>
     </div>
   </div>
@@ -86,7 +115,10 @@ export default {
                 margin-top: 24px;
                 height: 354px;
                 border: 1px dashed #DBDBDB;
-                padding: 30px 16px 28px 18px;
+                padding: 20px 20px 22px 20px;
+                >p {
+                  margin-bottom: 20px;
+                }
                 .price {
                   font-size: 18px;
                   color: #333;
@@ -95,6 +127,36 @@ export default {
                         font-size: 36px;
                         color: #DD2727;
                     }
+                }
+                .store_info {
+                  display: flex;
+                  justify-content: space-between;
+                }
+                // .earnest_money {
+                //   width: 365px;
+                //   display: flex;
+                //   justify-content: space-between;
+                // }
+                .earnest_money {
+                  span:first-child {
+                    margin-right: 136px;
+                  }
+                }
+                .groupon_info {
+                    display: flex;
+                    justify-content: space-between;
+                  }
+                .icons {
+                  display: flex;
+                  justify-content: space-between;
+                  font-size: 12px;
+                  color: #333;
+                  >p {
+                    >span {
+                      margin-right: 46px;
+                      padding-left: 18px;
+                    }
+                  }
                 }
             }
         }
@@ -123,6 +185,34 @@ export default {
         flex: 1;
         height: 500px;
         background-color: #fff;
+        .description_head {
+          height: 49px;
+          line-height: 49px;
+          display: flex;
+          font-size: 18px;
+          color: #333333;
+          border-bottom: 1px solid #F0F0F0;
+          >span {
+            padding-left: 24px;
+            padding-right: 24px;
+            background-color: #efe;
+            cursor: pointer;
+          }
+          span.active {
+            background-color: #FFC90F;
+            position: relative;
+          }
+          .active::after {
+            position: absolute;
+            content: '';
+            border-top: 10px solid #FFC90F;
+            border-bottom: 10px solid transparent;
+            border-right: 10px solid transparent;
+            border-left: 10px solid transparent;
+            bottom: -20px;
+            left: 50px;
+          }
+        }
       }
     }
 }
